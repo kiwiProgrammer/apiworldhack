53def192bd33bd92ed92b5bc34836179be7ed133
Mai Anh Vu <me@maianhvu.com>
2016-09-10 15:07:38 -0700

    add android project files

---
+++ app/src/main/java/com/integrateapiworld/hackathon/fashionex/MainActivity.java
@@ -0,0 +1,24 @@
+package com.integrateapiworld.hackathon.fashionex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = new Button();
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
